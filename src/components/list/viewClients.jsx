cd2687f19cebdab73607a8995a721bb271bc0033
Mónica <84694323+Acinom30@users.noreply.github.com>
2024-05-21 21:56:06 -0600

    Ingresa como cliente, entrenador o administrador

--- src/components/list/viewClients.jsx
+++ src/components/list/viewClients.jsx
@@ -7,156 +7,164 @@ import { confirmAlert } from 'react-confirm-alert';
 import 'react-confirm-alert/src/react-confirm-alert.css';
 import ToastifyError from '../ui/toastify/toastifyError';
 import ToastifySuccess from '../ui/toastify/toastifySuccess';
+import { useUser } from '../../userContext'
+
 
 const ViewClients = () => {
-    const [clients, setClients] = useState([]);
-    const [selectedClient, setSelectedClient] = useState(null);
-    const [searchTerm, setSearchTerm] = useState('');
-    const navigate = useNavigate();
-    const [triggerRefresh, setTriggerRefresh] = useState(false); 
+  const [clients, setClients] = useState([]);
+  const [selectedClient, setSelectedClient] = useState(null);
+  const [searchTerm, setSearchTerm] = useState('');
+  const navigate = useNavigate();
+  const { user } = useUser();
 
 
-  
-    useEffect(() => {
-      fetchClients();
-    }, [searchTerm]);
-  
-    const fetchClients = async () => {
-      const querySnapshot = await getDocs(collection(db, 'usuarios'));
-      const clientsData = querySnapshot.docs.map(doc => ({
-        id: doc.id,
-        ...doc.data(),
-      }));
-      const filteredClients = clientsData.filter(client => {
-        const normalize = (str) => str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
-        const fullName = normalize(`${client.primerNombre} ${client.primerApellido}`).toLowerCase();
-        const cedula = normalize(client.cedula).toLowerCase();
-        const searchTermLower = normalize(searchTerm).toLowerCase();
-        return fullName.includes(searchTermLower) || cedula.includes(searchTermLower);
-      });
-      setClients(filteredClients);
-    };
-          
 
-    const handleShowDetails = (client) => {
-      setSelectedClient(client);
-    };
+  useEffect(() => {
+    fetchClients();
+  }, [searchTerm]);
 
-    const handleEditClient = (client) => {
-      //setSelectedClient(client);
-      navigate('/userUpdate', { state: { client } });
-    };
+  const fetchClients = async () => {
+    const querySnapshot = await getDocs(collection(db, 'usuarios'));
+    const clientsData = querySnapshot.docs.map(doc => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+    const filteredClients = clientsData.filter(client => {
+      const normalize = (str) => str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+      const fullName = normalize(`${client.primerNombre} ${client.primerApellido}`).toLowerCase();
+      const cedula = normalize(client.cedula).toLowerCase();
+      const searchTermLower = normalize(searchTerm).toLowerCase();
+      return fullName.includes(searchTermLower) || cedula.includes(searchTermLower);
+    });
+    setClients(filteredClients);
+  };
 
-    const handleDeleteClient = async (client) => {
-      const nombre = client.primerNombre + ' ' + client.primerApellido
-      confirmAlert({
-        title: 'Confirmar Eliminación',
-        message: '¿Estás seguro de que deseas eliminar el cliente ' + nombre + '?',
-        buttons: [
-          {
-            label: 'Sí',
-            onClick: async () => {
-              try {
-                await deleteDoc(doc(db, 'usuarios', client.id));
-                ToastifySuccess('Cliente eliminado correctamente');
-                fetchClients(); // Llamamos a fetchClients directamente
-                navigate('/viewListClients');
-              } catch (error) {
-                ToastifyError('Error al eliminar el cliente');
-                console.error('Error al eliminar el cliente: ', error);
-              }
+
+  const handleShowDetails = (client) => {
+    setSelectedClient(client);
+  };
+
+  const handleEditClient = (client) => {
+    //setSelectedClient(client);
+    navigate('/userUpdate', { state: { client } });
+  };
+
+  const handleDeleteClient = async (client) => {
+    const nombre = client.primerNombre + ' ' + client.primerApellido
+    confirmAlert({
+      title: 'Confirmar Eliminación',
+      message: '¿Estás seguro de que deseas eliminar el cliente ' + nombre + '?',
+      buttons: [
+        {
+          label: 'Sí',
+          onClick: async () => {
+            try {
+              await deleteDoc(doc(db, 'usuarios', client.id));
+              ToastifySuccess('Cliente eliminado correctamente');
+              fetchClients(); // Llamamos a fetchClients directamente
+              navigate('/viewListClients');
+            } catch (error) {
+              ToastifyError('Error al eliminar el cliente');
+              console.error('Error al eliminar el cliente: ', error);
             }
-          },
-          {
-            label: 'No',
-            onClick: () => {}
           }
-        ]
-      });
-    };
-  
-    return (
-      <div className="container mx-auto p-4">
-        <Header/>
-        <h1 className="text-3xl font-bold text-center mb-4">Clientes</h1>
-        <div className="flex items-center justify-between mb-4">
+        },
+        {
+          label: 'No',
+          onClick: () => { }
+        }
+      ]
+    });
+  };
+
+  return (
+    <div className="container mx-auto p-4">
+      <Header />
+      <h1 className="text-3xl font-bold text-center mb-4">Clientes</h1>
+      <div className="flex items-center justify-between mb-4">
         <input
-            type="text"
-            placeholder="Buscar por nombre, apellido o cédula"
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-            className="border border-gray-300 p-2 rounded-md w-full"
+          type="text"
+          placeholder="Buscar por nombre, apellido o cédula"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          className="border border-gray-300 p-2 rounded-md w-full"
         />
         <button
-            onClick={() => setSearchTerm('')}
-            className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 ml-2 rounded"
+          onClick={() => setSearchTerm('')}
+          className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 ml-2 rounded"
         >
-            Limpiar
+          Limpiar
         </button>
-        </div>
-        <div className="overflow-x-auto">
-          <table className="table-fixed w-full">
-            <thead>
-              <tr>
-                <th className="w-1/4 px-4 py-2">Nombre</th>
-                <th className="w-1/4 px-4 py-2">Apellido</th>
-                <th className="w-1/4 px-4 py-2">Cédula</th>
-                <th className="w-1/4 px-4 py-2">Acciones</th>
-              </tr>
-            </thead>
-            <tbody>
-              {clients.map(client => (
-                <tr key={client.id} className='text-center'>
-                  <td className="border px-4 py-2">{client.primerNombre}</td>
-                  <td className="border px-4 py-2">{client.primerApellido}</td>
-                  <td className="border px-4 py-2">{client.cedula}</td>
-                  <td className="border px-4 py-2">
-                    <div className="inline-flex gap-5">
-                      <button
-                        onClick={() => handleShowDetails(client)}
-                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-3 rounded"
-                      >
-                        Ver Detalles
-                      </button>
-                      <button
-                        onClick={() => handleEditClient(client)}
-                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-3 rounded"
-                      >
-                        Editar
-                      </button>
+      </div>
+      <div className="overflow-x-auto">
+        <table className="table-fixed w-full">
+          <thead>
+            <tr>
+              <th className="w-1/4 px-4 py-2">Nombre</th>
+              <th className="w-1/4 px-4 py-2">Apellido</th>
+              <th className="w-1/4 px-4 py-2">Cédula</th>
+              <th className="w-1/4 px-4 py-2">Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            {clients.map(client => (
+              <tr key={client.id} className='text-center'>
+                {client.cedula !== '1' && (
+                  <>
+                    <td className="border px-4 py-2">{client.primerNombre}</td>
+                    <td className="border px-4 py-2">{client.primerApellido}</td>
+                    <td className="border px-4 py-2">{client.cedula}</td>
+                    <td className="border px-4 py-2">
+                      <div className="inline-flex gap-5">
                         <button
-                          onClick={() => handleDeleteClient(client)}
-                          className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-3 rounded"
+                          onClick={() => handleShowDetails(client)}
+                          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-3 rounded"
                         >
-                          Eliminar
+                          Ver Detalles
                         </button>
-                    </div>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-        {selectedClient && (
-          <div className="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50">
-            <div className="bg-white p-8 rounded shadow-lg max-w-md w-full">
-              <h2 className="text-2xl font-bold mb-4 text-center">
-                Detalles de {selectedClient.primerNombre} {selectedClient.primerApellido}
-              </h2>
-              <p><strong>Cédula:</strong> {selectedClient.cedula}</p>
-              <p><strong>Nombre Completo:</strong> {selectedClient.primerNombre} {selectedClient.segundoNombre}</p>
-              <p><strong>Apellidos:</strong> {selectedClient.primerApellido} {selectedClient.segundoApellido}</p>
-              <p><strong>Email:</strong> {selectedClient.email}</p>
-              <p><strong>Teléfono:</strong> {selectedClient.telefono}</p>
-              <p><strong>Observaciones/Enfermedades:</strong><br/> {selectedClient.observaciones}</p>
-              <div className="mt-4 flex justify-center">
-                <button onClick={() => setSelectedClient(null)} className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded">Cerrar</button>
-              </div>
+                        <button
+                          onClick={() => handleEditClient(client)}
+                          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-3 rounded"
+                        >
+                          Editar
+                        </button>
+                        {user.rol === 'administrador' && (
+                          <button
+                            onClick={() => handleDeleteClient(client)}
+                            className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-3 rounded"
+                          >
+                            Eliminar
+                          </button>
+                        )}
+                      </div>
+                    </td>
+                  </>
+                )}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      {selectedClient && (
+        <div className="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50">
+          <div className="bg-white p-8 rounded shadow-lg max-w-md w-full">
+            <h2 className="text-2xl font-bold mb-4 text-center">
+              Detalles de {selectedClient.primerNombre} {selectedClient.primerApellido}
+            </h2>
+            <p><strong>Cédula:</strong> {selectedClient.cedula}</p>
+            <p><strong>Nombre Completo:</strong> {selectedClient.primerNombre} {selectedClient.segundoNombre}</p>
+            <p><strong>Apellidos:</strong> {selectedClient.primerApellido} {selectedClient.segundoApellido}</p>
+            <p><strong>Email:</strong> {selectedClient.email}</p>
+            <p><strong>Teléfono:</strong> {selectedClient.telefono}</p>
+            <p><strong>Observaciones/Enfermedades:</strong><br /> {selectedClient.observaciones}</p>
+            <div className="mt-4 flex justify-center">
+              <button onClick={() => setSelectedClient(null)} className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded">Cerrar</button>
             </div>
           </div>
-        )}
-      </div>
-    );
-  };
-  
-  export default ViewClients;
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ViewClients;
